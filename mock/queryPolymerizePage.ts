2e82690f87de6265a799c500e60b345d16457ab0
wuhao <27276971@qq.com>
2023-07-12 16:00:44 +0800

    update

---
+++ mock/queryPolymerizePage.ts
@@ -0,0 +1,397 @@
+export default {
+  'POST /api/v1/pindao/polymerize/page/queryPolymerizePage': {
+    code: 0,
+    data: {
+      template: [
+        {
+          data: {
+            basicAttribute: {
+              monitor: {},
+            },
+            dataSource: {
+              topImage:
+                'https://bosscdn.otosaas.com/pro/20230510/20230510155328%E9%A1%B6%E9%83%A8%E8%83%8C%E6%99%AF%E5%9B%BE.png?path=0,1591',
+              name: '云饮品',
+              bodyStyle: {
+                backgroundColor: '#F9FAFB',
+              },
+            },
+          },
+          name: 'Header',
+          id: '94d0a397-54d3-4db0-abe9-29a8aa4e885e',
+          selected: false,
+          chosen: false,
+        },
+        {
+          data: {
+            relationStyle: 2,
+            basicAttribute: {
+              relationStyle: 2,
+              monitor: {},
+            },
+            dataSource: {
+              imgUrl: '',
+              name: '请输入您要搜索的关键',
+              pathUrl: '',
+            },
+          },
+          name: 'SearchInput',
+          id: 'f6e5c58a-9c4e-4cd4-aea6-c24d9d3e5601',
+          selected: false,
+          chosen: false,
+        },
+        {
+          data: {
+            basicAttribute: {
+              monitor: {
+                monitorName: '',
+                is: false,
+              },
+            },
+            dataSource: [
+              {
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20230625/20230625115336%E4%BA%91%E9%A5%AE%E5%93%81bannerbanner.png?path=0,1853',
+                name: '1',
+              },
+            ],
+          },
+          name: 'Carousel',
+          id: 'e28c33c0-3a78-45cc-9da4-64a2e556b5c2',
+          chosen: false,
+        },
+        {
+          data: {
+            backgroundColor: '#FFF6E7',
+            color: '#111A34',
+            relationStyle: 2,
+            basicAttribute: {
+              backgroundColor: '#FFFFFF',
+              color: '#111A34',
+              relationStyle: 2,
+              visibleName: 1,
+              monitor: {},
+            },
+            dataSource: [
+              {
+                iconLabel: '8.2折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20220930/20220930113608%E6%98%9F%E5%B7%B4%E5%85%8B.png?path=0,768',
+                name: '麦当劳',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230704163927190',
+              },
+              {
+                iconLabel: '9.3折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20220930/20220930113640%E5%96%9C%E8%8C%B6.png?path=0,769',
+                name: '喜茶',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627095059489',
+              },
+              {
+                iconLabel: '9折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20221020/20221020164530%E5%A5%88%E9%9B%AA%E7%9A%84%E8%8C%B6.png?path=0,802',
+                name: '奈雪的茶',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627100144251',
+              },
+              {
+                iconLabel: '5折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20220929/20220929194946Costa.png?path=0,761',
+                name: 'COSTA',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627100739805',
+              },
+              {
+                iconLabel: '8.9折起',
+                imgUrl: 'https://bosscdn.otosaas.com/pro/20220930/20220930114044DQ.png?path=0,772',
+                name: 'DQ',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627100240393',
+              },
+              {
+                iconLabel: '8.2折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20220930/20220930113837%E5%93%88%E6%A0%B9%E8%BE%BE%E6%96%AF.png?path=0,770',
+                name: '哈根达斯',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627100217732',
+              },
+              {
+                iconLabel: '7.1折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20221021/20221021143152%E5%BF%85%E8%83%9C%E5%AE%A2.png?path=0,808',
+                name: '必胜客',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627095230095',
+              },
+              {
+                iconLabel: '3.7折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20221021/20221021143320%E9%BA%A6%E5%BD%93%E5%8A%B3.png?path=0,809',
+                name: '麦当劳',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627092511475',
+              },
+              {
+                iconLabel: '6折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20221021/20221021111954%E7%91%9E%E5%B9%B8%E6%96%B0.png?path=0,807',
+                name: '瑞幸',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627100103962',
+              },
+              {
+                iconLabel: '6.3折起',
+                imgUrl: 'https://bosscdn.otosaas.com/pro/20221020/20221020165207kfc.png?path=0,803',
+                name: '肯德基',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627093300903',
+              },
+              {
+                iconLabel: '6.5折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20221021/20221021152710%E5%85%A8%E5%AE%B6.png?path=0,810',
+                name: '全家',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627094950356',
+              },
+              {
+                iconLabel: '8.8折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20230413/20230413170800%E7%BE%8E%E5%9B%A2.png?path=0,1494',
+                name: '美团',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627094751946',
+              },
+              {
+                iconLabel: '5.3折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20230413/20230413170932%E9%A5%BF%E4%BA%86%E4%B9%88.png?path=0,1495',
+                name: '饿了么',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627094657709',
+              },
+              {
+                iconLabel: '9.9折',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20230413/20230413171331%E7%99%BE%E6%9E%9C%E5%9B%AD.png?path=0,1496',
+                name: '百果园',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627094438958',
+              },
+              {
+                iconLabel: '7折起',
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20230518/202305181645381%EF%BC%9A1.png?path=0,1625',
+                name: '周黑鸭',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627094001717',
+              },
+            ],
+          },
+          name: 'BrandGroup',
+          id: '03af0e55-765f-44ce-ba3f-b22b4eb8472a',
+          selected: false,
+          chosen: false,
+        },
+        {
+          data: {
+            basicAttribute: {
+              monitor: {
+                monitorName: '',
+                is: false,
+              },
+            },
+            dataSource: [
+              {
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20230510/20230510160820%E8%85%B0%E9%83%A8.png?path=0,1597',
+                name: '腰部',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627093332944',
+              },
+            ],
+          },
+          name: 'Carousel',
+          id: '02300d5c-dff0-44c7-acee-e117f74eb4a4',
+          chosen: false,
+        },
+        {
+          data: {
+            basicAttribute: {
+              relationStyle: 2,
+              monitor: {
+                monitorName: '',
+                is: false,
+              },
+            },
+            dataSource: [
+              {
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20230510/20230510160411%E8%90%A5%E9%94%80%E4%BD%8D1.png?path=0,1594',
+                name: '1',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627092251322',
+              },
+              {
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20230510/20230510160438%E8%90%A5%E9%94%80%E4%BD%8D2.png?path=0,1595',
+                name: '2',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627095918910',
+              },
+              {
+                imgUrl:
+                  'https://bosscdn.otosaas.com/pro/20230510/20230510160456%E8%90%A5%E9%94%80%E4%BD%8D3.png?path=0,1596',
+                name: '3',
+                pathUrl: 'https://kuping.otosaas.com/kaquan/detail/C20230627095205633',
+              },
+            ],
+          },
+          name: 'Marketing',
+          id: 'ad976f8a-3ca1-4a49-ae76-400c55be7f1d',
+          chosen: false,
+        },
+        {
+          data: {
+            basicAttribute: {
+              activeLineStyle: {
+                backgroundColor: '#F93A4A',
+              },
+              color: '#41485D',
+              activeStyle: {
+                color: '#F93A4A',
+              },
+              relationStyle: 1,
+              monitor: {
+                componentName: '瀑布流',
+              },
+              stickyStyle: {
+                backgroundColor: '#FFFFFF',
+              },
+            },
+            dataSource: [
+              {
+                filter: 1,
+                name: '推荐',
+                recommendType: 5,
+                recommendCode: {
+                  createTime: '2023-06-27 10:18:29',
+                  num: 100,
+                  name: '推荐',
+                  updateTime: '2023-06-27 10:18:34',
+                  id: 10000494,
+                  appCode: 'kuping',
+                  status: 1,
+                },
+              },
+              {
+                filter: 1,
+                name: '咖啡生活',
+                recommendType: 5,
+                recommendCode: {
+                  createTime: '2023-06-27 10:21:54',
+                  num: 26,
+                  name: '人气好茶',
+                  updateTime: '2023-06-27 10:21:58',
+                  id: 10000495,
+                  appCode: 'kuping',
+                  status: 1,
+                },
+              },
+              {
+                filter: 1,
+                name: '视听会员',
+                recommendType: 5,
+                recommendCode: {
+                  createTime: '2023-06-27 10:23:54',
+                  num: 35,
+                  name: '视听会员',
+                  updateTime: '2023-06-27 10:23:58',
+                  id: 10000496,
+                  appCode: 'kuping',
+                  status: 1,
+                },
+              },
+              {
+                filter: 1,
+                name: '推荐套餐',
+                recommendType: 5,
+                recommendCode: {
+                  createTime: '2023-06-27 10:24:34',
+                  num: 11,
+                  name: '推荐套餐',
+                  updateTime: '2023-06-27 10:24:37',
+                  id: 10000497,
+                  appCode: 'kuping',
+                  status: 1,
+                },
+              },
+            ],
+          },
+          name: 'MasonryLayouts',
+          id: 'ed90f249-5a4b-4f63-8cff-674e2a5b42b5',
+          chosen: false,
+        },
+        {
+          data: {
+            activeColor: '#F93A4A',
+            color: '#858B9C',
+            backgroundImage: '',
+            basicAttribute: {
+              activeColor: '#F93A4A',
+              color: '#858B9C',
+              monitor: {},
+            },
+            dataSource: [
+              {
+                activeStyle: {
+                  backgroundImage:
+                    'https://bosscdn.otosaas.com/pro/20220930/20220930131141%E9%A6%96%E9%A1%B5%E9%80%89%E4%B8%AD.png?path=0,775',
+                },
+                name: '首页',
+                pathUrl: 'https://kuping.otosaas.com/template/P1673259790699057152',
+                style: {
+                  backgroundImage:
+                    'https://bosscdn.otosaas.com/pro/20220930/20220930131130%E9%A6%96%E9%A1%B5%E6%9C%AA%E9%80%89%E4%B8%AD.png?path=0,774',
+                },
+              },
+              {
+                activeStyle: {
+                  backgroundImage:
+                    'https://bosscdn.otosaas.com/pro/20220930/20220930131224%E8%AE%A2%E5%8D%95%E9%80%89%E4%B8%AD.png?path=0,777',
+                },
+                name: '订单',
+                pathUrl:
+                  'https://kuping.otosaas.com/orderv2/?orderType=kaquan&homeUri=template/P1673259790699057152',
+                style: {
+                  backgroundImage:
+                    'https://bosscdn.otosaas.com/pro/20220930/20220930131214%E8%AE%A2%E5%8D%95%E6%9C%AA%E9%80%89%E4%B8%AD.png?path=0,776',
+                },
+              },
+              {
+                activeStyle: {
+                  backgroundImage:
+                    'https://bosscdn.otosaas.com/pro/20220930/20220930131302%E6%88%91%E7%9A%84%E9%80%89%E4%B8%AD.png?path=0,779',
+                },
+                name: '我的',
+                pathUrl:
+                  'https://kuping.otosaas.com/userCenter/?orderType=kaquan&homeUri=template/P1673259790699057152',
+                style: {
+                  backgroundImage:
+                    'https://bosscdn.otosaas.com/pro/20220930/20220930131253%E6%88%91%E7%9A%84%E6%9C%AA%E9%80%89%E4%B8%AD.png?path=0,778',
+                },
+              },
+            ],
+          },
+          name: 'FooterNav',
+          id: 'ca239cd1-ac26-4491-960e-63e74597ee38',
+          chosen: false,
+        },
+      ],
+      createdAt: '2023-06-26 17:19:56',
+      polymerizeType: 1,
+      polymerizeCode: 'P1673259790699057152',
+      _id: {
+        date: '2023-06-26 17:19:56',
+        machineIdentifier: 10423833,
+        processIdentifier: 19794,
+        counter: 14832395,
+        time: 1687771196000,
+        timestamp: 1687771196,
+        timeSecond: 1687771196,
+      },
+      appCode: 'kuping',
+      version: 10,
+      polymerizeName: '云饮品',
+      status: 2,
+    },
+    message: '操作成功',
+  },
+};
